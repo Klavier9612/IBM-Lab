dbc99b333093b89bd97e86c5bd9af9f13813340a
Klavier9612 <124707000+Klavier9612@users.noreply.github.com>
2023-02-10 12:44:58 -0500

    Jupyter notebook uploaded

---
+++ IBM Lab.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# ##  **Cristian Andrés Riveros Cubillos**-  
+# Data Scientist
+
+# ### The following code is meant to perform a mathematic operation:
+
+# In[9]:
+
+
+print((3*3)*0.5)
+
+
+# In[ ]:
+
+
+- 1
+- 2
+
+
+# <img src="C:\Users\Cristian\Downloads" alt="Alt text" title="datascience-640" />
+
+# In[ ]:
+
+
+
+
